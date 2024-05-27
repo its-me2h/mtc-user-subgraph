99727346c8084dd81e0a8b46b6f5626171e0b9bf
its-me2h <harmouchet@gmail.com>
2024-05-27 19:55:35 +0100

    [DEV-9] Implement user-service Schema

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,2 @@
 export { User } from './user';
 export { Profile } from './profile';
-export { Account } from './account';
-export { Client } from './client';
-export { Manager } from './manager';
-export { Admin } from './admin';
